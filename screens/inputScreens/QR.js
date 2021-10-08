5d8b5ad595fce953c2459efe05f44f197684b37e
Kimseonmin <tjsals9506@naver.com>
2021-10-08 18:03:38 +0900

    Co-authored-by: TW-Lim <TW-Lim@users.noreply.github.com>

--- screens/inputScreens/QR.js
+++ screens/inputScreens/QR.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import QrScanner from '../../utils/camera';
 
 export default function QR() {
 	return (
 		<View
 			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
 		>
-			<Text>QR</Text>
+			{/*작업 확인용 임시 화면 */}
+			<QrScanner />
 		</View>
 	);
 }
