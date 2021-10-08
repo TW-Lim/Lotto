b24e919c03a7f504eb67aadac12b886ea66c6805
KangHyunMuk <201702039@tw.ac.kr>
2021-10-08 16:38:38 +0900

    홈 화면 재분할 및 테두리 작업 초안

--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+/* import './home.css'; */
 
 export default function home() {
 	return (
 		<View style={styles.container}>
 			<View style={styles.item1}>
-				<Text>로또</Text>
+				<Text style={styles.mainText}>981회</Text>
+				<Text>2021-09-18</Text>
 			</View>
 			<View style={styles.item2}>
+				<Text>메인</Text>
+			</View>
+
+			<View style={styles.item3}>
 				<Text>로그</Text>
 			</View>
 		</View>
@@ -17,18 +23,59 @@ export default function home() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		flexDirection: 'column', // 혹은 'column'
+		flexDirection: 'column', // 혹은 'row'
+		backgroundColor: 'white',
+		alignItems: 'center',
+		justifyContent: 'center',
 	},
+
+	mainText: {
+		fontSize: 40,
+		fontWeight: 'bold',
+	},
+
 	item1: {
+		flex: 0.5,
+		width: '83%',
+		backgroundColor: 'white',
+		justifyContent: 'center',
+		marginTop: '1%',
+	},
+
+	item2: {
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
-		backgroundColor: 'gray',
+		margin: 10,
+		marginBottom: '2%',
+		borderWidth: 1,
+		borderColor: '#c3c3c3',
+		borderRadius: 5,
+		width: '90%',
 	},
-	item2: {
+
+	item3: {
 		flex: 2,
 		alignItems: 'center',
 		justifyContent: 'center',
+		width: '90%',
 		backgroundColor: 'white',
+		margin: 10,
+		borderWidth: 1,
+		borderColor: '#c3c3c3',
+		borderRadius: 5,
+	},
+
+	circle: {
+		borderRadius: 50,
+		width: 40,
+		height: 40,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'red',
+	},
+
+	number: {
+		fontSize: 18,
 	},
 });
