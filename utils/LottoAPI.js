d43230f9afa188dae481c4c7deea177e64a02051
Lim <limlim@tw.ac.kr>
2021-10-11 20:23:30 +0900

    feature/LottoAPI async await 형태로 수정

--- utils/LottoAPI.js
+++ utils/LottoAPI.js
@@ -1,7 +1,7 @@
-function ReqWinNum(drawCount) {
+export const reqWinNum = async (drawCount) => {
 	const url =
 		'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=';
-
+	let json;
 	/* 
 	1. home에서 console.log, fetch를 변수에 할당 후 then(data)에서 리턴 - Promise._W에 데이터 수신 성공
 	1-1. home에서 console.log, then(data)에서 return 없이 data 호출 - Promise._W가 undefined
@@ -11,15 +11,13 @@ function ReqWinNum(drawCount) {
 
 	여기서 log하지 않고 외부 파일에서 로또정보객체만 사용하려면?
 	*/
-	let num = null;
-	fetch(`${url}${drawCount}`)
-		.then((response) => response.json())
-		.then((data) => {
-			num = data;
-			console.log(num, 'fetch');
-		});
-	setTimeout(() => console.log(num, 'LottoAPI'), 5000); //기다려야 정상적으로 num이 출력
-	return num; //함수 내에선 API 호출의 지연을 기다려주지만 외부파일에선 기다리지 않고 사용함
-}
+	try {
+		const response = await fetch(`${url}${drawCount}`);
+		json = await response.json();
+		jsonObj = await JSON.
+	} catch (e) {
+		console.error(e);
+	}
 
-export default ReqWinNum;
+	return json;
+};
