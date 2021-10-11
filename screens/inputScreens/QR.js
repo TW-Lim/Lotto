349daecb52777e445b1154a854b56b4237a2ab56
Lim <limlim@tw.ac.kr>
2021-10-12 04:46:56 +0900

    feature/camera 화면에 띄우기 성공

--- screens/inputScreens/QR.js
+++ screens/inputScreens/QR.js
@@ -3,12 +3,5 @@ import { View } from 'react-native';
 import QRScanner from '../../utils/camera';
 
 export default function QR() {
-	return (
-		<View
-			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-		>
-			{/*작업 확인용 임시 화면 */}
-			<QRScanner />
-		</View>
-	);
+	return <QRScanner />;
 }
