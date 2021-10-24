c0db1be2d0aa839b52c562d6fbf9d10142c70a44
Lim <limlim@tw.ac.kr>
2021-10-25 06:00:42 +0900

    FEAT - Number 채색 기능
    if문으로 children을 체크해서 color 변수에 알맞은 styles 객체를 담습니다
    색은 추후에 지금보다 적합한 다른 색으로 바꿔주세요
    
    당첨번호와 대조해 미 해당시 회색으로 나오는 기능은 Number 컴포넌트를 호출하는 컴포넌트에서 구현할 예정입니다

--- components/Number.js
+++ components/Number.js
@@ -3,8 +3,24 @@ import { Text, View, StyleSheet } from 'react-native';
 
 //변수를 children으로 받아 표시
 export default function Number(props) {
+	const number = props.children;
+	let color;
+
+	// children 크기에 따라 color변수에 알맞은 styles 객체 대입
+	if (number >= 1 && number < 11) {
+		color = styles.yellow;
+	} else if (number >= 11 && number < 21) {
+		color = styles.blue;
+	} else if (number >= 21 && number < 31) {
+		color = styles.red;
+	} else if (number >= 31 && number < 41) {
+		color = styles.black;
+	} else if (number >= 41) {
+		color = styles.green;
+	}
+
 	return (
-		<View style={styles.circle}>
+		<View style={[styles.circle, color]}>
 			<Text>{props.children}</Text>
 		</View>
 	);
@@ -18,5 +34,20 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		backgroundColor: '#ede0f0',
 		margin: '1%',
+	},
+	yellow: {
+		backgroundColor: 'yellow',
+	},
+	blue: {
+		backgroundColor: 'blue',
+	},
+	red: {
+		backgroundColor: 'red',
+	},
+	black: {
+		backgroundColor: 'grey',
+	},
+	green: {
+		backgroundColor: 'green',
 	},
 });
